21d069c6aa1851b555c01a3fd8535d28f8d2e045
Ahmet Esen <ahmetesen88@gmail.com>
2018-11-07 23:20:49 +0300

    initialCommit

---
+++ TahminEt/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Windows;
+namespace TahminEt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+            UserName.Text = Environment.UserName;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
